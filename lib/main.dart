be2dae33339839e2697b7e14a07f5b144ec56afe
Sarang <sarang98divakaran@gmail.com>
2020-11-25 18:51:44 +0530

    ✨ refactor: move home class to seperate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'pages/home.dart';
 
 void main() {
   runApp(App());
@@ -13,60 +14,7 @@ class App extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Home'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFFFFF2ED),
-      body: SafeArea(
-        top: true,
-        child: Center(
-          child: Text(
-            'Hello World',
-          ),
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-        elevation: 2.0,
-        backgroundColor: Color(0xFFF5855A),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              IconButton(
-                  icon: Icon(Icons.home),
-                  color: Color(0xFFF5855A),
-                  onPressed: () {}),
-              IconButton(
-                  icon: Icon(Icons.folder),
-                  color: Color(0xFFF5855A),
-                  onPressed: () {})
-            ],
-          ),
-        ),
-        shape: CircularNotchedRectangle(),
-      ),
+      home: HomePage(),
     );
   }
 }
---
+++ lib/pages/home.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFFF2ED),
+      body: SafeArea(
+        top: true,
+        child: Center(
+          child: Text(
+            'Home',
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        elevation: 2.0,
+        backgroundColor: Color(0xFFF5855A),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                  icon: Icon(Icons.home),
+                  color: Color(0xFFF5855A),
+                  onPressed: () {}),
+              IconButton(
+                  icon: Icon(Icons.folder),
+                  color: Color(0xFFF5855A),
+                  onPressed: () {})
+            ],
+          ),
+        ),
+        shape: CircularNotchedRectangle(),
+      ),
+    );
+  }
+}
