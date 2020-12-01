93c18a4ffca5926710027839051f918d0ab8602a
Sarang <sarang98divakaran@gmail.com>
2020-12-01 15:42:06 +0530

    🌟 feat: added router entry for user_input page

--- lib/router.dart
+++ lib/router.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:label_printer/pages/home.dart';
 import 'package:label_printer/pages/add.dart';
+import 'package:label_printer/pages/user_input.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
@@ -9,6 +10,13 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => HomePage());
     case '/add':
       return MaterialPageRoute(builder: (context) => AddItem());
+    case '/input':
+      var company = settings.arguments;
+      return MaterialPageRoute(
+        builder: (context) => UserInput(
+          company: company,
+        ),
+      );
     default:
       return MaterialPageRoute(builder: (context) => HomePage());
   }
