6770ed9ac0d4c5d83771a7891361377666238ec6
sarang <sarang98divakaran@gmail.com>
2025-04-20 13:58:52 +0530

    feat: added product and quantity fields

--- lib/pages/print_form.dart
+++ lib/pages/print_form.dart
@@ -8,10 +8,22 @@ class PrintFormPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // resizeToAvoidBottomInset: true,
-      appBar: AppBar(title: Text('Print', style: TextStyle(color: Color(0xFFF5855A)))),
-      body: _PrintForm(key: ValueKey(company.name), company: company),
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        debugPrint('touched');
+        // This is used to dismiss the keyboard when tapping outside of a text field
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: Scaffold(
+        // resizeToAvoidBottomInset: true,
+        appBar: AppBar(title: Text('Print')),
+        body: SafeArea(
+          child: Column(children: [Expanded(child: _PrintForm(key: ValueKey(company.name), company: company))]),
+        ),
+      ),
     );
   }
 }
@@ -26,8 +38,129 @@ class _PrintForm extends StatefulWidget {
 }
 
 class PrinterForm extends State<_PrintForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _quantityType = 'Weight';
+  String? _selectedUnit = 'g';
+
+  final TextEditingController _productName = TextEditingController();
+  final TextEditingController _quantity = TextEditingController();
+
+  final FocusNode _productNameFocus = FocusNode();
+  final FocusNode _quantityFocus = FocusNode();
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Simple Text Widget', style: TextStyle(fontSize: 20, color: Colors.black)));
+    return Form(
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      key: _formKey,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Product Name
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                spacing: 8,
+                children: [
+                  Text('Product Name', style: Theme.of(context).textTheme.labelLarge),
+                  TextFormField(
+                    focusNode: _productNameFocus,
+                    onChanged: (value) => setState(() {}),
+                    decoration: InputDecoration(
+                      suffixIcon:
+                          _productNameFocus.hasFocus && _productName.text.isNotEmpty
+                              ? IconButton(
+                                icon: Icon(Icons.clear),
+                                onPressed: () {
+                                  _productName.clear();
+                                },
+                              )
+                              : null,
+                    ),
+                    controller: _productName,
+                  ),
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                spacing: 10,
+                children: [
+                  Text('Quantity Type', style: Theme.of(context).textTheme.labelLarge),
+                  Expanded(
+                    child: DropdownButtonFormField<String>(
+                      value: _quantityType,
+                      items: [
+                        DropdownMenuItem<String>(value: 'Weight', child: Text('Weight')),
+                        DropdownMenuItem<String>(value: 'Count', child: Text('Count')),
+                        DropdownMenuItem<String>(value: 'None', child: Text('None')),
+                      ],
+                      onChanged: (val) {
+                        setState(() {
+                          _quantityType = val as String;
+                          if (val == 'None') {
+                            _quantity.clear();
+                          }
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            // Quantity
+            if (_quantityType != 'None')
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: 8,
+                  children: [
+                    Text(_quantityType, style: Theme.of(context).textTheme.labelLarge),
+                    TextFormField(
+                      focusNode: _quantityFocus,
+                      onChanged: (value) => setState(() {}),
+                      decoration: InputDecoration(
+                        enabled: _quantityType != 'None',
+                        suffixIcon:
+                            _quantityType == 'Weight'
+                                ? Row(
+                                  mainAxisSize: MainAxisSize.min, // Ensures the row takes minimal space
+                                  children: [
+                                    DropdownButton<String>(
+                                      value: _selectedUnit,
+                                      isDense: true,
+                                      underline: const SizedBox(), // Removes the default underline
+                                      items: const [
+                                        DropdownMenuItem(value: 'g', child: Text('gram (g)')),
+                                        DropdownMenuItem(value: 'kg', child: Text('kilogram (kg)')),
+                                      ],
+                                      onChanged: (value) {
+                                        setState(() {
+                                          _selectedUnit = value; // Update the selected unit
+                                        });
+                                      },
+                                    ),
+                                  ],
+                                )
+                                : null,
+                      ),
+                      controller: _quantity,
+                    ),
+                  ],
+                ),
+              ),
+
+            // Horizontal Divider
+            Padding(padding: const EdgeInsets.all(16.0), child: const Divider(height: 1, thickness: 1)),
+          ],
+        ),
+      ),
+    );
   }
 }
